55426a04ba093ecd58df4f553cf0c4704888f6c9
miliisanchezz <milisanchezcaruso@gmail.com>
2025-11-20 16:57:42 -0300

    Proyecto

---
+++ POO/ProyectoTurnera/Gui/RegistroPaciente.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace ProyectoTurnera.Gui
+{
+    partial class RegistroPaciente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonCargarPaciente = new System.Windows.Forms.Button();
+            this.textBoxObraSocial = new System.Windows.Forms.TextBox();
+            this.textBoxDNI = new System.Windows.Forms.TextBox();
+            this.textBoxApellidoPaciente = new System.Windows.Forms.TextBox();
+            this.textBoxNombrePaciente = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Sitka Heading", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(333, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 36);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Atrás";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Sitka Heading", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(189, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 21);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Obra Social";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Sitka Heading", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(190, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 21);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "DNI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Sitka Heading", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(189, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 21);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Apellido";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Heading", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(190, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 21);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Nombre";
+            // 
+            // buttonCargarPaciente
+            // 
+            this.buttonCargarPaciente.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonCargarPaciente.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.buttonCargarPaciente.Font = new System.Drawing.Font("Sitka Heading", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCargarPaciente.Location = new System.Drawing.Point(333, 277);
+            this.buttonCargarPaciente.Name = "buttonCargarPaciente";
+            this.buttonCargarPaciente.Size = new System.Drawing.Size(70, 36);
+            this.buttonCargarPaciente.TabIndex = 15;
+            this.buttonCargarPaciente.Text = "Ingresar";
+            this.buttonCargarPaciente.UseVisualStyleBackColor = false;
+            // 
+            // textBoxObraSocial
+            // 
+            this.textBoxObraSocial.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxObraSocial.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.textBoxObraSocial.Location = new System.Drawing.Point(353, 215);
+            this.textBoxObraSocial.Name = "textBoxObraSocial";
+            this.textBoxObraSocial.Size = new System.Drawing.Size(259, 25);
+            this.textBoxObraSocial.TabIndex = 14;
+            this.textBoxObraSocial.Text = "Ingrese obra social";
+            // 
+            // textBoxDNI
+            // 
+            this.textBoxDNI.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDNI.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.textBoxDNI.Location = new System.Drawing.Point(353, 176);
+            this.textBoxDNI.Name = "textBoxDNI";
+            this.textBoxDNI.Size = new System.Drawing.Size(259, 25);
+            this.textBoxDNI.TabIndex = 13;
+            this.textBoxDNI.Text = "Ingrese DNI";
+            // 
+            // textBoxApellidoPaciente
+            // 
+            this.textBoxApellidoPaciente.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxApellidoPaciente.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.textBoxApellidoPaciente.Location = new System.Drawing.Point(353, 139);
+            this.textBoxApellidoPaciente.Name = "textBoxApellidoPaciente";
+            this.textBoxApellidoPaciente.Size = new System.Drawing.Size(259, 25);
+            this.textBoxApellidoPaciente.TabIndex = 12;
+            this.textBoxApellidoPaciente.Text = "Ingrese apellido";
+            // 
+            // textBoxNombrePaciente
+            // 
+            this.textBoxNombrePaciente.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNombrePaciente.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.textBoxNombrePaciente.Location = new System.Drawing.Point(353, 99);
+            this.textBoxNombrePaciente.Name = "textBoxNombrePaciente";
+            this.textBoxNombrePaciente.Size = new System.Drawing.Size(259, 25);
+            this.textBoxNombrePaciente.TabIndex = 11;
+            this.textBoxNombrePaciente.Text = "Ingrese nombre";
+            // 
+            // RegistroPaciente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonCargarPaciente);
+            this.Controls.Add(this.textBoxObraSocial);
+            this.Controls.Add(this.textBoxDNI);
+            this.Controls.Add(this.textBoxApellidoPaciente);
+            this.Controls.Add(this.textBoxNombrePaciente);
+            this.Name = "RegistroPaciente";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonCargarPaciente;
+        private System.Windows.Forms.TextBox textBoxObraSocial;
+        private System.Windows.Forms.TextBox textBoxDNI;
+        private System.Windows.Forms.TextBox textBoxApellidoPaciente;
+        private System.Windows.Forms.TextBox textBoxNombrePaciente;
+    }
+}
---
+++ POO/ProyectoTurnera/Gui/Ver.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ProyectoTurnera.Gui
+{
+    partial class Ver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCargarPaciente = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // buttonCargarPaciente
+            // 
+            this.buttonCargarPaciente.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonCargarPaciente.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.buttonCargarPaciente.Font = new System.Drawing.Font("Sitka Heading", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCargarPaciente.Location = new System.Drawing.Point(453, 349);
+            this.buttonCargarPaciente.Name = "buttonCargarPaciente";
+            this.buttonCargarPaciente.Size = new System.Drawing.Size(70, 36);
+            this.buttonCargarPaciente.TabIndex = 10;
+            this.buttonCargarPaciente.Text = "Ingresar";
+            this.buttonCargarPaciente.UseVisualStyleBackColor = false;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(262, 279);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(138, 17);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Ver medicos disponibles";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(262, 231);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(138, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ver medicos disponibles";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(262, 188);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(128, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ver turnos disponibles";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Ver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCargarPaciente);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Ver";
+            this.Text = "Ver";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCargarPaciente;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
